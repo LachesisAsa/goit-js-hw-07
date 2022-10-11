2d153843e811fa7a8777b9049fbf82fad3a952ef
Serhii Artyshchuk <serban9999@gmail.com>
2022-10-11 09:22:15 +0300

    homework is ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+ const galleryMarkup = galleryItems.map((eItems) => {
+    return ` <div class="gallery__item">
+    <a 
+        class="gallery__link" 
+        href="${eItems.original}">
+      <img
+        class="gallery__image"
+        src="${eItems.preview}"
+        data-source="${eItems.original}"
+        alt="${eItems.description}"
+      />
+    </a>
+  </div>`
+})
+//  console.log(galleryItems);
+//  console.log(galleryMarkup);
+const artGallery = document.querySelector(".gallery");
+artGallery.insertAdjacentHTML("afterbegin", galleryMarkup.join(""));
+// console.log(artGallery);
 
-console.log(galleryItems);
+let modal;
+function clickElemGallPict () {
+    document.addEventListener("keydown", escListener);
+}
+artGallery.addEventListener("click", (evt) => {
+    const clickTarget = evt.target;
+    if(!clickTarget.classList.contains("gallery__image")){
+        return;
+    }
+    const imageSrc = clickTarget.dataset.sourse;
+    const imageAlt = clickTarget.alt;
+    modal = basicLightbox.create(`<img src="${imageSrc}" alt="${imageAlt}" />`);
+    modal.show();
+    clickElemGallPict();
+});
+function clickElemGallPictOff (){
+    documen.remuveEventListener("keydown", escListener);
+}
+function escListener(evt) {
+    if( evt.key === "Escape"){
+        modal.close();
+        clickElemGallPictOff();
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems.map((picture) => {
+  return `<li>
+    <a class="gallery__item" 
+        href="${picture.original}" 
+        onclick="event.preventDefault()">
+      <img class="gallery__image" 
+        src="${picture.preview}" 
+        alt="${picture.description}" />
+    </a>
+    </li>`;
+});
+
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("afterbegin", galleryMarkup.join(""));
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+console.log(lightbox);
